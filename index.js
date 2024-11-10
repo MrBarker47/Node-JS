ddb1f4ce692be21355769616ac935b1a056df81a
MrBarker47 <jebarker7@gmail.com>
2024-11-10 14:27:13 -0500

    Update files

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
+let http = require('http');
+let hostname = '127.0.0.1';
+let port = 3000;
 
-const server = http.createServer((req, res) => {
+let server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.write('<h1 style="color: red">Hello World!</h1>')
